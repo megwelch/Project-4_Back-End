fdea47ce0fe75fb3c3088dc3b58c8f35b5bd2f60
megwelch <meghanmwelch@gmail.com>
2022-11-22 20:20:22 -0500

    favorites route working

--- app/models/tvshows.js
+++ app/models/tvshows.js
@@ -1,30 +1,7 @@
 const mongoose = require('mongoose')
 
-const showSchema = new mongoose.Schema(
-	{
-		title: {
-			type: String,
-		},
-		image: {
-			type: String,
-		},
-		rating: {
-			type: String,
-			
-		},
-		cast: {
-			type: Array,
-		
-		},
-		description: {
-			type: String,
-			
-		},
-		owner: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'User',
-		}
-	}
+const tvShowSchema = new mongoose.Schema(
+
 )
 
-module.exports = mongoose.model('Show', showSchema)
+module.exports = mongoose.model('Show', tvShowSchema)
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 // require route files
 const reviewRoutes = require('./app/routes/review_routes')
 const userRoutes = require('./app/routes/user_routes')
+const tvShowRoutes = require('./app/routes/tvshow_routes')
 
 // require middleware
 const errorHandler = require('./lib/error_handler')
@@ -67,6 +68,7 @@ app.use(requestLogger)
 // register route files
 app.use(reviewRoutes)
 app.use(userRoutes)
+app.use(tvShowRoutes)
 
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
